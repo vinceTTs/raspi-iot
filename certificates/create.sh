68c8540b07041089c4e98a65a3672ae975077caa
Vincent Dörr <vincent@reimovinc.de>
2026-01-07 09:42:09 +0100

    INFLUX_TOKEN warning, single cert

--- certificates/create.sh
+++ certificates/create.sh
@@ -1,5 +1,25 @@
-NAME="telegraf"
-openssl genrsa -out $NAME.key 2048
-openssl req -new -key $NAME.key -out $NAME.csr -subj "/CN=$NAME"
-openssl x509 -req -in $NAME.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out $NAME.crt -sha256
+#!/bin/bash
 
+# Prüfen, ob ein Name übergeben wurde
+if [ -z "$1" ]; then
+  echo "Usage: $0 <NAME>"
+  exit 1
+fi
+
+NAME="$1"
+
+echo "Erzeuge Zertifikat für: $NAME"
+
+# Private Key
+openssl genrsa -out "$NAME.key" 2048
+
+# CSR
+openssl req -new -key "$NAME.key" -out "$NAME.csr" -subj "/CN=$NAME"
+
+# Signiertes Zertifikat
+openssl x509 -req -in "$NAME.csr" -CA ca.crt -CAkey ca.key -CAcreateserial -out "$NAME.crt" -sha256
+
+echo "Fertig:"
+echo "  $NAME.key"
+echo "  $NAME.csr"
+echo "  $NAME.crt"
